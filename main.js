13725e5f56cdf674a4450613e9e1294983748b01
bardleware <devan.sisson@vivintsolar.com>
2017-04-28 11:10:19 -0600

    added background,foreground, and enemy sprites

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ var currentState,
     frames = 0,
     sgroup,
     slimeEnme,
+    gameWidth,
+    foregroundPosition = backgroundPosition = 0,
+    foreGroundMove = 2,
     thehero;
-
+var groundLevel = 180;
 var states = {
     Splash: 0,
     Game: 1,
@@ -14,40 +17,40 @@ var states = {
 var canvas;
 var renderingContext;
 
-function SlimeGroup(){
+function SlimeGroup() {
     this.collection = [];
 
-    this.reset = function(){
+    this.reset = function () {
         this.collection = [];
     }
 
-    this.add = function(){
+    this.add = function () {
         this.collection.push(new Slime());
     }
 
-    this.update = function(){
-        if(frames % 100 === 0){ //Add an slime every 100 frames
+    this.update = function () {
+        if (frames % 123 === 0) {
             this.add();
         }
 
-        for(var i = 0, len = this.collection.length; i < len; i++){
+        for (var i = 0, len = this.collection.length; i < len; i++) {
             var slime = this.collection[i];
 
-            if(i === 0){
+            if (i === 0) {
                 slime.detectCollision();
             }
 
             slime.x -= 2;
-            if(slime.x < -slime.width){
+            if (slime.x < -slime.width) {
                 this.collection.splice(i, 1);
-                i --;
-                len --;
+                i--;
+                len--;
             }
         }
     }
 
-    this.draw = function(){
-        for (var i = 0, len = this.collection.length; i < len; i++){
+    this.draw = function () {
+        for (var i = 0, len = this.collection.length; i < len; i++) {
             var slime = this.collection[i];
             slime.draw();
         }
@@ -55,85 +58,87 @@ function SlimeGroup(){
 }
 
 function Slime() {
-    this.x = 400;
-    this.y = 340;
+    this.x = gameWidth;
+    this.y = 300 + Math.floor(Math.random() * 33);
+    this.frame = 15;
+    this.rotation = 0;
     this.width = slimeEnme.width;
     this.height = slimeEnme.height;
 
-    this.detectCollision = function(){
+    this.detectCollision = function () {
         //console.log(this.x + "-the player is at : " + (thehero.x + thehero.width));
         console.log(thehero.y);
-        if (this.x <= (thehero.x + thehero.width) && this.x >= thehero.x && thehero.y >= 135){
+        if (this.x <= (thehero.x + thehero.width) && this.x >= thehero.x && thehero.y >= 135) {
 
             //console.log("you're dead");
             currentState = states.Score;
         }
     }
 
-    this.draw = function(){
+    this.draw = function () {
         slimeEnme.draw(renderingContext, this.x, this.y);
     }
 }
 
-function Hero(){
-    this.x = 50;
-    this.y = 200;
+function Hero() {
+    this.x = 60;
+    this.y = groundLevel;
     this.width = 45;
     this.height = 55;
 
     this.frame = 0;
     this.velocity = 0;
-    this.animation = [0,1,2,1];
+    this.animation = [0, 1, 2, 1];
 
     this.rotation = 0;
     this.radius = 12;
 
     this.gravity = 0.25;
-    this._jump = 5.6;
-    this.jumpcount = 2;
+    this._jump = 6;
+    this.jumpcount = 3;
 
-    this.jump = function() {
-        if(this.jumpcount > 0){
+    this.jump = function () {
+        if (this.jumpcount > 0) {
             this.velocity = -this._jump;
-            this.jumpcount --;
+            this.jumpcount--;
         }
     }
 
-    this.update = function(){
+    this.update = function () {
         var h = currentState === states.Splash ? 10 : 5;
         //console.log(h + "hero h rate");
         this.frame += frames % h === 0 ? 1 : 0;
         this.frame %= this.animation.length;
         //console.log(this.frame);
 
-        if(currentState === states.Splash){
+        if (currentState === states.Splash) {
             this.updateIdleHero();
         }
-        else{
+        else {
             this.updatePlayingHero();
         }
 
     }
 
-    this.updateIdleHero = function(){
+    this.updateIdleHero = function () {
         //this.y = 250;
     }
 
-    this.updatePlayingHero = function(){
+    this.updatePlayingHero = function () {
         this.velocity += this.gravity;
         this.y += this.velocity;
 
         // check to see if hit the ground and stay there
-        if(this.y >= 160){
+        if (this.y >= groundLevel) {
             //console.log("hit ground");
-            this.y = 160;
+            this.y = groundLevel;
             this.jumpcount = 2;
             this.velocity = this._jump;
         }
 
     }
 
-    this.draw = function(renderingContext){
+    this.draw = function (renderingContext) {
         renderingContext.save();
 
         renderingContext.translate(this.x, this.y);
@@ -148,7 +153,7 @@ function Hero(){
 }
 
 
-function main(){
+function main() {
     windowSetup();
     canvasSetup();
     currentState = states.Splash;
@@ -159,16 +164,16 @@ function main(){
     sgroup = new SlimeGroup();
 }
 
-function windowSetup(){
-  var inputEvent = "touchstart";
-  var windowWidth = $(window).width();
-  console.log(windowWidth);
-    if(windowWidth < 500){
-        width = 320;
+function windowSetup() {
+    var inputEvent = "touchstart";
+    var windowWidth = $(window).width();
+    console.log(windowWidth);
+    if (windowWidth < 500) {
+        gameWidth = width = 320;
         height = 430;
     }
-    else{
-        width = 700;
+    else {
+        gameWidth = width = 700;
         height = 500;
         inputEvent = "mousedown";
     }
@@ -176,13 +181,13 @@ function windowSetup(){
     document.addEventListener(inputEvent, onpress);
 }
 
-function onpress(evt){
+function onpress(evt) {
     //console.log("click happened");
 
-    switch (currentState){
+    switch (currentState) {
         case states.Splash:
-            thehero.jump();
             currentState = states.Game;
+            foreGroundMove = 3;
             break;
         case states.Game:
             thehero.jump();
@@ -190,7 +195,7 @@ function onpress(evt){
     }
 }
 
-function canvasSetup(){
+function canvasSetup() {
     canvas = document.createElement("canvas");
     canvas.style.border = "3px solid black";
     canvas.width = width;
@@ -198,11 +203,11 @@ function canvasSetup(){
     renderingContext = canvas.getContext("2d");
 }
 
-function loadGraphics(){
+function loadGraphics() {
     var img = new Image();
-    img.src = "SpriteSheet_old.png";
-    img.onload = function(){
-        initSprites(this);
+    img.src = "SpriteSheet.png";
+    img.onload = function () {
+        initSprites(img);
         renderingContext.fillStyle = "#cccccc";
 
 
@@ -213,24 +218,34 @@ function loadGraphics(){
 
 }
 
-function gameLoop(){
+function gameLoop() {
     update();
     render();
     window.requestAnimationFrame(gameLoop);
 }
 
-function update(){
-    frames ++;
-    if(currentState === states.Game){
+function update() {
+    frames++;
+
+    if (currentState !== states.Score) {
+        backgroundPosition = (backgroundPosition - 1) % 700;
+        foregroundPosition = (foregroundPosition - foreGroundMove) % 350; // Move left two px each frame. Wrap every 14px.
+    }
+
+    if (currentState === states.Game) {
         sgroup.update();
     }
     thehero.update();
 }
 
-function render(){
+function render() {
     renderingContext.fillRect(0, 0, width, height);
-    //backgroundSprite.draw(renderingContext, 0, 180);
-    //slimeEnme.draw(renderingContext, 220, 340);
+    backgroundSprite.draw(renderingContext, backgroundPosition, 0);
+    backgroundSprite.draw(renderingContext, backgroundPosition + backgroundSprite.width, 0);
     sgroup.draw(renderingContext);
     thehero.draw(renderingContext);
+
+    foregroundSprite.draw(renderingContext, foregroundPosition, 451);
+    foregroundSprite.draw(renderingContext, foregroundPosition + foregroundSprite.width, 451);
+    foregroundSprite.draw(renderingContext, foregroundPosition + foregroundSprite.width*2, 451);
 }
--- sprite.js
+++ sprite.js
@@ -1,5 +1,6 @@
 var link;
 var slimeEnme;
+var bgModifierX= 1280;
 
 function Sprite(img, x, y, width, height){
   this.img = img;
@@ -16,13 +17,49 @@ Sprite.prototype.draw = function(context, x, y){
 function initSprites(img){
   //link = new Sprite(img, 105, 0, 45, 55);
   nolan = [
-    new Sprite(img, 0, 245, 73, 91),
-    new Sprite(img, 73, 245, 73, 91),
-    new Sprite(img, 146, 245, 73, 91)
+    new Sprite(img, 1280, 245, 73, 91),
+    new Sprite(img, 1353, 245, 73, 91),
+    new Sprite(img, 1426, 245, 73, 91)
   ];
-  slimeEnme = [
-    new Sprite(img, 0, 47, 50, 27),
-    new Sprite(img, 0, 97, 51, 25),
-  ]
+  slimeEnme = new Sprite(img, 1280, 0, 48, 147);
+
+  backgroundSprite = new Sprite(img, 400, 100, 700, 500);
+  foregroundSprite = new Sprite(img, 0, 720, 350, 60);
 
 }
+// if (windowWidth < 500) {
+//         gameWidth = width = 320;
+//         height = 430;
+//     }
+//     else {
+//         gameWidth = width = 700;
+//         height = 500;
+//         inputEvent = "mousedown";
+//     }
+
+
+// class Octorok {
+//     constructor() {
+//         this.x = 400;
+//         this.y = 300; //+ Math.floor(Math.random()*50);
+//         this.frame = 0;
+//         this.animation = [0, 1, 2, 1];
+//         this.rotation = 0;
+//         this.width = octorokSprite[0].width;
+//         this.height = octorokSprite[0].height;
+
+//         this.detectCollision = function () {
+//             if (this.x <= (hero.x + hero.width - 3) && this.x >= hero.x && (this.height + 153) <= (hero.y + hero.height)) {
+//                 console.log("You're dead");
+//                 currentState = states.Splash;
+//             }
+//         }
+
+//         this.draw = function () {
+//             var h = this.animation[this.frame];
+
+
+//             octorokSprite[h].draw(renderingContext, this.x, this.y);
+//         }
+//     }
+// }
